4762e3b1dec4e3245dbb94bc490a9a1d42fc8e4d
david <davidtucu@hotmail.com>
2022-05-29 16:53:42 -0300

    hooks useState TextInput onChangeText defaultValue

--- App.js
+++ App.js
@@ -1,32 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import react, {useState} from 'react';
+import { StyleSheet, Text, View , TextInput, Dimensions} from 'react-native';
+import  { useState } from 'react';
 
-const TextpState = () => {
-  const [texto, setTexto]= useState("Hola State")
-  const actualizaTexto = () =>
-  {
-    setTexto("Chao State")
-  }
-  return <Text style={[styles.red,{fontSize: 24}]} onPress={actualizaTexto}>{texto}</Text>
-}
+const width = Dimensions.get('window').width
 
 const Texto = (props) => {
-  const {texto} = props;
-  return <Text style={[styles.text, styles.blue]}>{texto}</Text>
-}
-const Texto2 = (props) => {
   const {children} = props;
   return <Text style={styles.green}>{children}</Text>
 }
 
 export default function App() {
+  const [textito, setTextito] = useState('chanchito feliz');
   return (
     <View style={styles.container}>
-      <Texto texto={"Hola Mundo"}></Texto>
-      <Texto2>Chao Mundo</Texto2>
-      <TextpState></TextpState>
-      <StatusBar style="auto" />
+      <Texto>{textito}</Texto>
+      <TextInput style={styles.input} placeholder='Escribir aqui' onChangeText={t => setTextito(t)} defaultValue={textito} ></TextInput>
     </View>
   );
 }
@@ -36,40 +23,21 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 24,
     height:100,
-    // width:100
   },
-  // 3 + 2 + 1 = 6
-  red:{ // 1/6
- //   flex:1,
-    backgroundColor: 'red'
-  },
-  green:{
-  //  flex:2, // 2/6 = 1/3
-    backgroundColor: 'green'
-  },
-  blue:{
-   // flex:3, //  3/6 = 1/2
-    backgroundColor: 'blue'
+  input:{
+    height: 40,
+    backgroundColor: '#eee',
+    borderBottomWidth: 1,
+    // width: '100%',
+    width: width,
   },
   container: {
     flex: 1,
-   // flexDirection:  'column-reverse',  mueve el contenido direccion vertical invertida
-    flexDirection:  'column',//  mueve el contenido direccion vertical
-  //   flexDirection:  'row-reverse', mueve el contenido direccion horizontal invertida
-   //  flexDirection: 'row', mueve el contenido direccion horizontal
+    flexDirection:  'column',
     
     backgroundColor: '#fff',
     
-   justifyContent: 'center', // acomoda el contenido en el centro
-   // justifyContent: 'flex-start' // acomoda el contenido en el inicio arriba
-    // justifyContent: 'flex-end' // acomoda el contenido en el final abajo
-    // justifyContent: 'space-between' // acomoda los elemntos con un espaciado entre ellos
-    // justifyContent: 'space-around'  // acomoda los elemntos con un espaciado entre ellos sea mas equitativo
-    // justifyContent: 'space-evenly' // acomoda los elemntos con un espaciado entre ellos 
-    // alignItems: 'center', // alinar los contenidos de izquierda a derecha, tal cual como justifycontent 
-    // alignItems: 'flex-start' // alinar los contenidos al inicio
-    // alignItems: 'baseline'
-    alignItems: 'stretch' //trat de usar el maximo ancho
-    // alignItems: 'flex-end',
+   justifyContent: 'center', 
+    alignItems: 'center', 
   }
 });
