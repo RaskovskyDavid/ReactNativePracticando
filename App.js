ced2ca1e7fb89a56eeaff4b4bc0c779b374e0635
david <davidtucu@hotmail.com>
2022-06-04 15:15:46 -0300

    ActivityIndicator

--- App.js
+++ App.js
@@ -1,50 +1,30 @@
-import { StyleSheet, Text, View, SectionList } from 'react-native';
-import  { useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import  { useState, useEffect } from 'react';
 
 
 
 export default function App() {
+ 
   return (
     <View style={styles.container}>
-     <SectionList
-     sections={[
-       {
-         title: 'Grupo1',
-       data: [{key: '1', nombre: 'David'},
-       {key: '2', nombre: 'Ariel'},
-       {key: '3', nombre: 'Raskovsky'},
-       {key: '4', nombre: 'Capo'},
-       {key: '5', nombre: 'Muy'},]
-      },
-      {
-        title: 'Grupo2',
-        data: [{key: '6', nombre: 'David'},
-        {key: '7', nombre: 'Ariel'},
-        {key: '8', nombre: 'Raskovsky'},
-        {key: '9', nombre: 'Capo'},
-        {key: '10', nombre: 'Muy'},]
-      }
-      ]}
-      renderItem={({ item}) => <Text style={styles.item}>{item.nombre}</Text>}
-      renderSectionHeader={({section}) => <Text style={styles.section}>{section.title}</Text>}
-      >
-
-     </SectionList>
+    <ActivityIndicator 
+    size="large"
+    color="#000fff"
+    />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  section: {
-fontSize: 16,
-fontWeight: 'bold'
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
- item: {
-   padding:10,
+ item: {padding:10,
   
   borderBottomColor: '#ccc',
-  borderBottomWidth: 1
-},
+borderBottomWidth: 1},
   container: {
     flex: 1,
     flexDirection:  'column',
