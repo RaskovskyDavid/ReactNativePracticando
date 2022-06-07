15843fea477b2f9fe7ac5a0a23501b0a75a14191
david <davidtucu@hotmail.com>
2022-06-07 17:50:34 -0300

    Alert

--- App.js
+++ App.js
@@ -1,31 +1,23 @@
-import { StyleSheet, View, Modal, Text, Button } from 'react-native';
+import { StyleSheet, View,   Button , Alert} from 'react-native';
 import  { useState, useEffect } from 'react';
+// import { Alert } from 'react-native-web';
 
-
+const crearDialogo = ()=> Alert.alert(
+  'Titulo',
+   'subtitulo o mensaje',
+   [
+     {
+       text:'Cancelar',
+       onPress: () => {},
+     }
+   ]
+)
 
 export default function App() {
- const [modal, setModal] = useState(false)
 
   return (
     <View style={styles.container}>
-    <Modal
-    // animationType='fade' // aparece lentamente 
-    // animationType='none' // aparece lentamente 
-    animationType='slide' // aparece Deslizandose desde fuera de la pantalla 
-    transparent={true}
-    visible={modal}
-    >
-      <View
-        style={styles.center}
-      >
-        <View style={styles.content}>
-          <Text>Soy un Modal</Text>
-          <Button title='Cerrar modal' onPress={()=> setModal(!modal)}></Button>
-        </View>
-      </View>
-      
-    </Modal>
-    <Button title='Abrir modal' onPress={() => setModal(!modal)} />
+    <Button title='Abrir dialogo' onPress={crearDialogo} />
     </View>
   );
 }
